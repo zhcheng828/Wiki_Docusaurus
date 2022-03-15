72c0460c26498fdbef19085fd6255f37d4ee9584
zhcheng <zhcheng828@gmail.com>
2022-03-15 19:29:30 +0800

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,13 +24,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/zhcheng828/Wiki_Docusaurus/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/zhcheng828/Wiki_Docusaurus/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
